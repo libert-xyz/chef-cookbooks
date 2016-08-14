9bb8be53a27d8a5de6e7eaf50a581664c270ca1b
Libert R Schmidt <rschmidt@nuvops.com>
2016-08-14 13:10:41 -0400

    django,wp

---
+++ cookbooks/wp/README.md
@@ -0,0 +1,4 @@
+# wp
+
+TODO: Enter the cookbook description here.
+
---
+++ cookbooks/wp/metadata.rb
@@ -0,0 +1,10 @@
+name 'wp'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures wp'
+long_description 'Installs/Configures wp'
+version '0.1.0'
+
+
+depends cookbook 'mysql', '= 7.2.0'
